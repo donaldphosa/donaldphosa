f358b72e854d6d9b06ca3b22416be0fc9488e0a1
donald phosa <86829073+donaldphosa@users.noreply.github.com>
2022-09-03 13:12:24 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import  {createNativeStackNavigator}  from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+import Auth from './pages/Auth';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import VerifyPin from './pages/VerifyPin'
+import PhoneNumber from './pages/PhoneNumber';
+import Home from './pages/Home';
+
+
+export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName='Auth' screenOptions={ { headerShown: false }} >
+            <Stack.Screen name='Auth' component={Auth}/>
+            <Stack.Screen name='SignUp' component={SignUp}/>
+            <Stack.Screen name='Login' component={Login}/>
+            <Stack.Screen name='PhoneNumber' component={PhoneNumber}/>
+            <Stack.Screen name='VerifyPin' component={VerifyPin}/>
+            <Stack.Screen name='Home' component={Home}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+     
+    
+  );
+}
+
